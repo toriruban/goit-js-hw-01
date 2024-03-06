8524b969efe15df9445abb09268d746f74eb5a06
ToriRuban <toriruban@icloud.com>
2024-03-06 23:07:50 +0200

    update task 1

--- js/task-1.js
+++ js/task-1.js
@@ -5,10 +5,3 @@ function makeTransaction (quantity, pricePerDroit){
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-
-function makeMessage (name, price) {
-    const message = `You picked ${name}, price per item is ${price} credits`;
-      // Change code above this line
-      return message;
-    };
